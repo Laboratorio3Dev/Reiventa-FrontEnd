a79b344c00a06336bf1c318af4889f661465f3fb
Scott Perez <scott.perez@gestionysistemas.com>
2026-01-04 07:17:09 -0500

    se agrego el admin

---
+++ WebBackOffice/DTO/Oficinas/ProductoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebBackOffice.DTO.Oficinas
+{
+    public class ProductoDTO
+    {
+        public int IdProducto { get; set; }
+        public string? Titulo { get; set; }
+        public string? Asunto { get; set; }
+        public string? SubTitulo { get; set; }
+        public int? Orden { get; set; }
+    }
+}
